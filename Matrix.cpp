0173f8da98ba03294c768f82cd8ae5a0c997f678
hadoth <pokomeda.karol@gmail.com>
2017-11-04 17:38:00 +0100

    Git init

---
+++ Matrix.cpp
@@ -0,0 +1,95 @@
+#include <cstring>
+#include <stdexcept>
+#include <iostream>
+#include "Matrix.h"
+
+
+Matrix::Matrix() {
+    m = 3;
+    n = 3;
+    makeEmpty();
+}
+
+Matrix::Matrix(unsigned int rows, unsigned int columns) {
+    m = rows;
+    n = columns;
+    makeEmpty();
+}
+
+Matrix::Matrix(Matrix &matrixToCopy) {
+    m = matrixToCopy.m;
+    n = matrixToCopy.n;
+
+    structure = new int *[m];
+
+    for (int i = 0; i < m; i++) {
+        structure[i] = new int[n];
+        memcpy(structure[i], matrixToCopy.structure[i], n * sizeof(int));
+    }
+}
+
+Matrix::Matrix(Matrix &&matrixToMove) {
+    m = matrixToMove.m;
+    n = matrixToMove.n;
+    structure = matrixToMove.structure;
+
+    matrixToMove.m = 0;
+    matrixToMove.n = 0;
+    matrixToMove.structure = nullptr;
+}
+
+Matrix::~Matrix() {
+    for (int i = 0; i < m; i++) {
+        delete structure[i];
+    }
+
+    delete structure;
+}
+
+void Matrix::setValue(unsigned int row, unsigned int column, int value) {
+    structure[row][column] = value;
+}
+
+int Matrix::getValue(unsigned int row, unsigned int column) {
+    return structure[row][column];
+}
+
+Matrix Matrix::add(Matrix matrixToAdd) {
+    Matrix result(matrixToAdd.m, matrixToAdd.n);
+
+    if (matrixToAdd.m != m || matrixToAdd.n != n) {
+        throw std::invalid_argument("Matrix dimensions do not match!");
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result.structure[i][j] = structure[i][j] + matrixToAdd.structure[i][j];
+        }
+    }
+
+    return result;
+}
+
+void Matrix::print() {
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            std::cout << "[" << structure[i][j] << "]";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Matrix::makeEmpty() {
+    structure = new int *[m];
+
+    for (int i = 0; i < m; i++) {
+        structure[i] = new int[n];
+        memset(structure[i], 0, n * sizeof(int));
+    }
+}
+
+
+
+
+
+
