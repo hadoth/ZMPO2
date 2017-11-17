7ad38cf91d12b43c2f3ee8359dc1d6e7defc1b56
hadoth <pokomeda.karol@gmail.com>
2017-11-17 07:18:57 +0100

    Minor refactor; provide doc for copy and move constructors

--- Matrix.cpp
+++ Matrix.cpp
@@ -16,6 +16,18 @@ Matrix::Matrix(unsigned int rows, unsigned int columns) {
     makeEmpty();
 }
 
+/**
+ * Konstruktor kopiujący
+ *
+ * Konstruktor ten tworzy niezależną kopię oryginalnego obiektu. Jest wykorzystywany wszędzie tam gdzie
+ * niezbędne jest utworzenie identycznego, nieodróżnialnego obiektu, a nie jest dla użytkownika istotne, czy
+ * istnieje więcej niż jedna instancja danego obiektu. Wszelkie zdefiniowane dla danego obiektu funkcje i operatory
+ * porównujące (equals, comparator, "=") powinny traktować oryginał i kopię obiektu jako tożsame. Ponadto, żadna ze zmian
+ * dokonanych na oryginale nie powinna wpływać na kopię i odwrotnie, żadna ze zmian dokonanych na kopii nie powinna
+ * wpływać na oryginał.
+ *
+ * @param matrixToCopy
+ */
 Matrix::Matrix(Matrix &matrixToCopy) {
     m = matrixToCopy.m;
     n = matrixToCopy.n;
@@ -29,6 +41,21 @@ Matrix::Matrix(Matrix &matrixToCopy) {
         }
     }
 }
+
+/**
+ * Konstruktor przenoszący
+ *
+ * Konstruktor ten operuje na tzw. rvalue. Efektem działania konstruktora przenoszącego jest nowy obiekt będący
+ * nieodróżnialny od oryginału przekazanego w konstruktorze. Wspomniany oryginał powinien natomiast przejść w stan neutralny,
+ * to znaczy taki, który nie pozwala na dostęp do żadnej z uprzednio przechowywanych danych oraz nie wpływa na dalszą
+ * pracę programu. Konstruktor przenoszący wykorzystywany jest głównie poprzez standardowe biblioteki c++, np. funkcja
+ * move i struktura vector. Konstruktor przenoszący powinien być wykorzystywany wszędzie tam, gdzie niezbędne jest
+ * przypisanie obiektu do innej zmiennej, a niewskazane jest  tworzenie nowych instancji obiektów (np. utworzenie obiektu
+ * z wykorzystaniem domyślnego konstruktora jest czasochłonne), lub instancjonowanie/destrukcja obiektów wiąże się
+ * z dodatkowymi operacjami wpływającymi na pracę innych części programu (np. tworzenie i zwalnianie "locków" na zasoby).
+ *
+ * @param matrixToMove
+ */
 
 Matrix::Matrix(Matrix &&matrixToMove) {
     m = matrixToMove.m;
@@ -75,12 +102,14 @@ Matrix Matrix::add(Matrix matrixToAdd) {
 }
 
 Matrix Matrix::multiply(int scalar) {
-    Matrix result(*this);
+    Matrix result(this->m, this->n);
+
     for (int i = 0; i < result.m; i++) {
         for (int j = 0; j < result.n; j++) {
-            result.structure[i][j] *= scalar;
+            result.structure[i][j] = this->structure[i][j] * scalar;
         }
     }
+
     return result;
 }
 
@@ -89,6 +118,7 @@ Matrix Matrix::multiply(Matrix otherMatrix) {
         throw std::invalid_argument
                 ("Matrix dimensions do not match; for A x B, A's columns count must match B's rows count");
     }
+
     Matrix result(this->getRows(),otherMatrix.getColumns());
 
     for (int i = 0; i < result.m; i++) {
@@ -126,6 +156,7 @@ void Matrix::validate(unsigned int row, unsigned int column){
     if (row < 0 || row >= this->m) {
         throw std::out_of_range("Row value exceeds matrix dimensions");
     }
+
     if (column < 0 || column >= this->n) {
         throw std::out_of_range("Column value exceeds matrix dimensions");
     }
